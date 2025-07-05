203a48d0de4152ba0818d20af4082e420db17ef7
ken <you@example.com>
2025-07-05 20:26:13 +0900

    zshrc

--- container/claudecode/setup.sh
+++ container/claudecode/setup.sh
@@ -29,6 +29,18 @@ export ANTHROPIC_SMALL_FAST_MODEL=us.anthropic.claude-3-5-haiku-20241022-v1:0
 . /home/ec2-user/.nvm/nvm.sh
 EOF
 
+cat <<EOF >> /home/ec2-user/.zshrc
+# Claude codee 
+# Bedrockを使う設定
+export CLAUDE_CODE_USE_BEDROCK=1
+
+# Amazon Bedrock (モデルIDを使用)
+export ANTHROPIC_MODEL=us.anthropic.claude-3-7-sonnet-20250219-v1:0
+#export ANTHROPIC_MODEL=us.anthropic.claude-sonnet-4-20250514-v1:0
+export ANTHROPIC_SMALL_FAST_MODEL=us.anthropic.claude-3-5-haiku-20241022-v1:0
+. /home/ec2-user/.nvm/nvm.sh
+EOF
+
 . ~/.bashrc
 envsubst < add_mcp.json > add_token_github_mcp.json
 claude mcp add-json github-org "$(cat add_token_github_mcp.json)" --verbose
