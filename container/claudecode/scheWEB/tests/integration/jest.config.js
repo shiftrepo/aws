15ec228a4c7b10de41c64e22a6e97957be77352e
Cloud User <ec2-user@ip-10-0-1-86.ec2.internal>
2025-10-01 04:29:15 +0000

    Revert "#8 planning"
    This reverts commit f2f4c05d9bca2ee1594e2caf84a1f08d84055273.

--- container/claudecode/scheWEB/tests/integration/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  displayName: 'Integration Tests',
-  testEnvironment: 'node',
-  testMatch: ['**/tests/integration/**/*.test.js'],
-  setupFilesAfterEnv: ['<rootDir>/tests/integration/setup.js'],
-  testTimeout: 30000,
-  verbose: true,
-  maxWorkers: 1, // Run serially to avoid database conflicts
-};
